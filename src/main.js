9092846e84112e3245064b5d805c7aa6f2c0a8d2
Julia Jaworska <ulia012008@gmail.com>
2025-04-04 11:05:26 +0200

    accordion

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+import '@fontsource/inter-tight/400.css';
+import '@fontsource/inter-tight/500.css';
+import '@fontsource/inter-tight/600.css';
+import '@fontsource/inter-tight/700.css';
+import '@fontsource/inter-tight/800.css';
+import '@fontsource/inter-tight/900.css';
+// зайві потім приберем
 
+import { initAccordion } from './js/about-me';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initAccordion();
+});
