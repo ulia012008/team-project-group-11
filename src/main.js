ea6e3e4153daa845a6cccebc4e4871b332e9604f
Julia Jaworska <ulia012008@gmail.com>
2025-04-05 23:35:44 +0200

    main.js

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import '@fontsource/inter-tight/400.css';
+import '@fontsource/inter-tight/500.css';
+import '@fontsource/inter-tight/600.css';
+import '@fontsource/inter-tight/700.css';
+import '@fontsource/inter-tight/800.css';
+import '@fontsource/inter-tight/900.css';
+
+// зайві потім приберем
+
+// about me Accordion start
+
+import { initAccordion } from './js/about-me-acc';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initAccordion();
+});
+import { AccordionFaq } from './js/faq.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  AccordionFaq();
+});
+
+// finish about me Accordion
+
+// swipe
+import { initSkillsSliderAbout } from './js/about-me-swipe';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSkillsSliderAbout();
+});
+// finishswipe
+
+// FAQ
+
+import { init } from './js/reviews.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  init();
+});
