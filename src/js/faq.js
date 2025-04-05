68cb1ad572c66aa746a9e893d316510a257b0290
alina <alinkash2811@gmail.com>
2025-04-05 14:49:56 +0200

    faq update js

--- src/js/faq.js
+++ src/js/faq.js
@@ -8,10 +8,18 @@ export function AccordionFaq() {
 
     buttonsFaq.forEach(button => {
         button.addEventListener("click", () => {
-            const panelFaq = button.nextElementSibling;
+            const panelFaq = button.parentElement.nextElementSibling;
             panelFaq.classList.toggle("open");
+            
+            //   console.log(panelFaq);  // Check the panel
 
-            const arrowFaq = button.querySelectorAll(".open-arrow-icon");
+            // if (panelFaq) {  // Make sure panel exists before toggling
+            //     panelFaq.classList.toggle("open");
+            // } else {
+            //     console.error("Panel not found for button", button);
+            // }
+
+            const arrowFaq = button.querySelector(".open-arrow-icon");
             arrowFaq.classList.toggle("rotated");
 
         })
