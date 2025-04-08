e10369d27303b73e353c65b28a415d46214ce878
Julia Jaworska <ulia012008@gmail.com>
2025-04-08 18:04:10 +0200

    js header

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import '@fontsource/inter-tight/800.css';
 import '@fontsource/inter-tight/900.css';
 
 // зайві потім приберем
-// import { initHeader } from './js/header.js';
+import { initHeader } from './js/header.js';
 import { initAccordion } from './js/about-me-acc';
 import { initSkillsSliderAbout } from './js/about-me-swipe.js';
 import { AccordionFaq } from './js/faq.js';
@@ -44,8 +44,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // const buttonElemLeft = document.getElementById('btn-left');
-  // const buttonElemRight = document.getElementById('btn-right');
+  const buttonElemLeft = document.getElementById('btn-left');
+  const buttonElemRight = document.getElementById('btn-right');
 
-  // initHeader(buttonElemLeft, buttonElemRight);
+  initHeader(buttonElemLeft, buttonElemRight);
 });
