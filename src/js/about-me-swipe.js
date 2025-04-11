26ddb7c69b01f101abe38c6eaec206e5e40a5f16
Julia Jaworska <ulia012008@gmail.com>
2025-04-11 12:48:48 +0200

    Update about-me-swipe.js

--- src/js/about-me-swipe.js
+++ src/js/about-me-swipe.js
@@ -2,10 +2,11 @@ import Swiper from 'swiper';
 import 'swiper/css';
 
 export function initSkillsSliderAbout() {
-  new Swiper('.skills-swiper-about-me', {
+  const swiperAbout = new Swiper('.skills-swiper-about-me', {
     slidesPerView: 2,
     loop: true,
-    spaceBetween: 0,
+    cssMode: true,
+    watchSlidesProgress: true,
     navigation: {
       nextEl: '.swiper-button-next-about-me',
     },
@@ -22,4 +23,26 @@ export function initSkillsSliderAbout() {
       },
     },
   });
+
+  updateActiveCircleAbout(swiperAbout);
+  swiperAbout.slideTo(0, 0);
+
+  swiperAbout.on('slideChange', () => {
+    updateActiveCircleAbout(swiperAbout);
+  });
+}
+
+function updateActiveCircleAbout(swiperAbout) {
+  const circlesAbout = document.querySelectorAll('.swiper-slide .circle-about-me');
+  circlesAbout.forEach(circleAbout => {
+    circleAbout.classList.remove('red-about-me');
+    circleAbout.classList.add('outline-about-me');
+  });
+
+  const activeSlideAbout = swiperAbout.slides[swiperAbout.activeIndex];
+  const activeCircleAbout = activeSlideAbout.querySelector('.circle-about-me');
+  if (activeCircleAbout) {
+    activeCircleAbout.classList.remove('outline-about-me');
+    activeCircleAbout.classList.add('red-about-me');
+  }
 }
